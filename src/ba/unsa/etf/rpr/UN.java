fa599364f74e6b8f307833a04524ff177f537a7d
Nermin Krdzic <nkrdzic1@etf.unsa.ba>
2018-12-03 17:20:53 +0100

    Kreirao klasu UN

---
+++ src/ba/unsa/etf/rpr/UN.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+
+public class UN {
+    private ArrayList<Drzava> drzave = new ArrayList<>();
+
+    public ArrayList<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+}
